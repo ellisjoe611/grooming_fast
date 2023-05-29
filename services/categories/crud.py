70872a7be5063e1fe2d2beb81561e4f026ff6ea4
Jong-Hyuk Ellis Cho <ellisjoe611@gmail.com>
2023-05-29 14:58:44 +0900

    fix: category 수정

--- services/categories/crud.py
+++ services/categories/crud.py
@@ -7,5 +7,5 @@ def get_category_mst(db_session: Session) -> list[schema.CategoryMst]:
     return db_session.query(model.CategoryMst).all()
 
 
-def get_category_dtl(db_session: Session, cat_mst_cd: str | None = None) -> list[schema.CategoryDtl]:
+def get_category_dtl(db_session: Session, cat_mst_cd: str) -> list[schema.CategoryDtl]:
     return db_session.query(model.CategoryDtl).filter(model.CategoryDtl.cat_mst_cd == cat_mst_cd).all()
